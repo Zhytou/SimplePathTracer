5aa4473315a3c58d0892307209ef9381037118d7
Zhytou <zhoytou@gmail.com>
2023-01-07 19:55:36 +0800

    update

--- src/Triangle.hpp
+++ src/Triangle.hpp
@@ -90,6 +90,47 @@ class Triangle {
     return;
   };
 
+  // 与光线求交 只不过在之前res的基础上比较
+  void hitCompare(const Ray& ray, HitResult& res) const {
+    if (!res.isHit) {
+      return;
+    }
+
+    Vec3<float> origin = ray.getOrigin();
+    Vec3<float> direction = ray.getDirection();
+    Vec3<float> normal = this->normal;
+
+    if (dot(normal, direction) > 0) {
+      // 入射光线与法线必须呈钝角，但后续判断交点是否在三角形内部，需要使用原生法向量
+      normal = -normal;
+    }
+
+    if (dot(normal, direction) == 0) {
+      return;
+    }
+
+    float t = (dot(normal, p1) - dot(normal, origin)) / dot(normal, direction);
+    if (t < 0.001 || t >= res.distance) {
+      return;
+    }
+
+    Vec3<float> p = ray.pointAt(t);
+    Vec3<float> c1 = cross(p2 - p1, p - p1);
+    Vec3<float> c2 = cross(p3 - p2, p - p2);
+    Vec3<float> c3 = cross(p1 - p3, p - p3);
+    if (dot(c1, this->normal) < 0 || dot(c2, this->normal) < 0 ||
+        dot(c3, this->normal) < 0) {
+      return;
+    }
+
+    res.isHit = true;
+    res.hitPoint = p;
+    res.normal = normal;
+    res.distance = t;
+    res.material = material;
+    return;
+  };
+
   void printStatus() {
     std::cout << "vertex 1: " << p1.x << '\t' << p1.y << '\t' << p1.z << '\n'
               << "vertex 2: " << p2.x << '\t' << p2.y << '\t' << p2.z << '\n'
