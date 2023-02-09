0932ccd1d488e3f80a7af68dc378cee688c76e3b
Yang Zhong <56868292+Zhytou@users.noreply.github.com>
2023-02-09 17:47:27 +0800

    add BVH tree initialization method and tree node cmp method

--- src/BVH.hpp
+++ src/BVH.hpp
@@ -11,11 +11,51 @@ class BVHNode : Hittable {
   AABB aabb;
 
  public:
-  BVHNode(const std::vector<Triangle>& triangles) {}
+  BVHNode(Hittable *object) : left(object), right(object) {}
+  BVHNode(const std::vector<Hittable*>& objects, int low, int high) {
+    assert(!objects.empty() && 0 <= low && low < high && high <= object.size());
+
+    sort(objects.begin(), objects.end(), zCmp);
+    if (objects.size() == 1) {
+      left = objects[0];
+      right = objects[0];
+      // TODO: add AABB method which allows Hittable* construction 
+    } else {
+      int mid = low + (high - low) / 2;
+      left = new BVHNode(objects, low, mid);
+      right = new BVHNode(objects, mid, high);
+      aabb = AABB::getSurroundingAABB(left->getAABB(), right->getAABB());
+    }
+
+  }
 
  private:
   // TODO: cmp for BVHNode
+  static bool xCmp(Hittable *left, Hittable *right) {
+    AABB laabb = left->getAABB();
+    AABB raabb = right->getAABB();
 
+    return laabb.getMin().x < raabb.getMax().x;
+  }
+
+  static bool yCmp(Hittable *left, Hittable *right) {
+    AABB laabb = left->getAABB();
+    AABB raabb = right->getAABB();
+
+    return laabb.getMin().y < raabb.getMax().y;
+  }
+
+  static bool zCmp(Hittable *left, Hittable *right) {
+    AABB laabb = left->getAABB();
+    AABB raabb = right->getAABB();
+
+    return laabb.getMin().z < raabb.getMax().z;
+  }
+
+ public:
+  // getter.
+  AABB getAABB() const {return aabb;}
+  
  public:
   virtual void hit(const Ray& ray, HitResult& res) const override {
     aabb.hit(ray, res);
