f69bbb6327248815c1f60a8a82b3a6535e165941
Zhytou <zhoytou@gmail.com>
2023-02-15 23:17:27 +0800

    texture support

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -11,7 +11,10 @@ Texture* Texture::getInstance(const std::string& texName) {
     Texture* ntex = new Texture();
     ntex->img = cv::imread(texName, cv::IMREAD_COLOR);
     textures[texName] = ntex;
-
+    // std::cout << "texture image: " << ntex->img.cols << '\t' <<
+    // ntex->img.rows
+    //           << '\t' << ntex->img.channels() << '\t' << ntex->img.depth()
+    //           << std::endl;
     cv::namedWindow(texName.c_str(), 0);
     cv::imshow(texName.c_str(), ntex->img);
     cv::waitKey(0);
@@ -29,9 +32,19 @@ Vec3<float> Texture::getColorAt(const Vec2<float>& pos) {
   assert(pos.u >= 0 && pos.u <= 1 && pos.v >= 0 && pos.v <= 1);
   Vec3<float> color(0, 0, 0);
   int rows = img.rows - 1, cols = img.cols - 1;
-  color.x = img.at<cv::Vec3f>(pos.u * rows, pos.v * cols)[2];
-  color.y = img.at<cv::Vec3f>(pos.u * rows, pos.v * cols)[1];
-  color.z = img.at<cv::Vec3f>(pos.u * rows, pos.v * cols)[0];
+  switch (img.depth()) {
+    case CV_8U:
+      color.x = img.at<cv::Vec3b>(pos.u * rows, pos.v * cols)[2] / 255.0f;
+      color.y = img.at<cv::Vec3b>(pos.u * rows, pos.v * cols)[1] / 255.0f;
+      color.z = img.at<cv::Vec3b>(pos.u * rows, pos.v * cols)[0] / 255.0f;
+      break;
+    default:
+      color.x = img.at<cv::Vec3f>(pos.u * rows, pos.v * cols)[2];
+      color.y = img.at<cv::Vec3f>(pos.u * rows, pos.v * cols)[1];
+      color.z = img.at<cv::Vec3f>(pos.u * rows, pos.v * cols)[0];
+      break;
+  }
+
   return color;
 }
 
