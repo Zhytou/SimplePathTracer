19024abf825bd62b74e817e7cbca6062647997aa
Zhytou <zhoytou@gmail.com>
2023-03-22 00:16:07 +0800

    ray

--- src/Ray.cpp
+++ src/Ray.cpp
@@ -22,16 +22,8 @@ Ray Ray::standardRefractRay(const Vec3<float> &point,
   float sineRefraction = sineIncidence / refraction;
   float cosineRefraction = sqrt(1 - sineRefraction * sineRefraction);
 
-  // std::cout << cosineIncidence << '\t' << sineIncidence << '\t'
-  //           << sineRefraction << '\t' << cosineRefraction << '\n';
-
   Vec3<float> directionParallelN = normal * Vec3<float>::dot(normal, direction);
   Vec3<float> directionVerticalN = direction - directionParallelN;
-
-  // std::cout << "Paralle d:" << directionParallelN.x << '\t'
-  //           << directionParallelN.y << '\t' << directionParallelN.z << '\n';
-  // std::cout << "Vertical d:" << directionVerticalN.x << '\t'
-  //           << directionVerticalN.y << '\t' << directionVerticalN.z << '\n';
 
   Vec3<float> ndirectionParallelN(0, 0, 0);
   if (cosineIncidence != 0) {
@@ -51,12 +43,8 @@ Ray Ray::standardRefractRay(const Vec3<float> &point,
 Ray Ray::standardReflectRay(const Vec3<float> &point,
                             const Vec3<float> &direction,
                             const Vec3<float> &normal) {
-  // 分解入射光
-  Vec3<float> directionParallelN = normal * Vec3<float>::dot(normal, direction);
-  Vec3<float> directionVerticalN = direction - directionParallelN;
-  // 反射光
-  Vec3<float> ndirection = -directionParallelN + directionVerticalN;
-  return Ray(point, ndirection);
+  float cosine = Vec3<float>::dot(normal, direction);
+  return Ray(point, direction - normal * 2 * cosine);
 }
 
 // 漫反射光线
