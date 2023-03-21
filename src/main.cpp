ca48e7c350b4430152c5523041fbf33beeef9544
Zhytou <zhoytou@gmail.com>
2023-03-22 00:16:20 +0800

    add save image function

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,15 @@ int main() {
   Tracer t(3, 5);
   // t.loadExampleScene();
   // t.load("../example/box/", "box.obj", "box.xml");
-  t.load("../example/veach-mis/", "veach-mis.obj", "veach-mis.xml");
-  // t.load("../example/cornell-box/", "cornell-box.obj", "cornell-box.xml");
+  // t.load("../example/veach-mis/", "veach-mis.obj", "veach-mis.xml");
+  t.load("../example/cornell-box/", "cornell-box.obj", "cornell-box.xml");
   // t.load("../example/staircase/", "stairscase.obj", "staircase.xml");
   auto img = t.render();
   imshow(windName, img);
 
-  waitKey(0);
+  if (waitKey(0) == static_cast<int>('s')) {
+    imwrite("cornell-box.bmp", img);
+  }
   destroyWindow(windName);
   return 0;
 }
