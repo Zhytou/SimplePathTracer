30ac87c9e594d7c33bd5a63b922c371bd5793cf6
Zhytou <zhoytou@gmail.com>
2025-05-30 22:53:48 +0800

    add sampleGGX and sampleCosine for bxdf samping

--- include/Random.hpp
+++ include/Random.hpp
@@ -2,12 +2,17 @@
 #define SRE_RANDOM_HPP
 
 #include <cstdlib>
+#include "Vec.hpp"
 
 namespace spt {
 
 int randInt(int max, int min = 0);
 
 float randFloat(float max, float min = 0);
+
+Vec3<float> sampleCosine();
+
+Vec3<float> sampleGGX(float roughness);
 
 }  // namespace spt
 
--- src/Random.cpp
+++ src/Random.cpp
@@ -8,4 +8,31 @@ float randFloat(float max, float min) {
   return min + 1.0 * rand() / RAND_MAX * (max - min);
 }
 
+Vec3<float> sampleCosine() {
+  float a = randFloat(1), b = randFloat(1);
+  float cosTheta = sqrtf(a);
+  float sinTheta = sqrtf(1 - cosTheta * cosTheta);
+  float Phi =  2 * PI * b;
+  float x = cosf(Phi) * sinTheta;
+  float y = sinf(Phi) * sinTheta;
+  float z = cosTheta;
+
+  return Vec3<float>(x, y, z);
+}
+
+Vec3<float> sampleGGX(float roughness) {
+  float alpha = roughness * roughness;
+  float alpha2 = alpha * alpha;
+
+  float a = randFloat(1), b = randFloat(1);
+  float cosTheta = sqrtf((1 - a) / (1 + (alpha2 - 1) * a));
+  float sinTheta = sqrtf(1 - cosTheta * cosTheta);
+  float Phi = 2 * PI * b;
+  float x = cosf(Phi) * sinTheta;
+  float y = sinf(Phi) * sinTheta;
+  float z = cosTheta;
+
+  return Vec3<float>(x, y, z);
+}
+
 }  // namespace spt
