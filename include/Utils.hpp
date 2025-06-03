efd8cf795855530eabfdb6f7e06065a37face07c
Zhytou <zhoytou@gmail.com>
2025-06-03 17:42:38 +0800

    rm random.hpp and add utils.hpp 将Vec和Random头文件中函数功能合并为Utils

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -1,9 +1,14 @@
-#ifndef SRE_VEC_HPP
-#define SRE_VEC_HPP
+#ifndef SRE_UTILS_HPP
+#define SRE_UTILS_HPP
 
+#include <cassert>
 #include <cmath>
+#include <string>
+#include <vector>
 #include <ostream>
-#include <functional>
+#include <sstream>
+#include <type_traits>
+#include <random>
 
 #define PI M_PI
 
@@ -159,6 +164,34 @@ template<typename T>
 std::ostream& operator<<(std::ostream& os, const Vec3<T>& v) {
   os << "Vec3(" << v.x << ", " << v.y << ", " << v.z << ")";
   return os;
+}
+
+template<typename T>
+T rand(T max, T min = 0) {
+  static_assert(std::is_arithmetic<T>::value, "T must be numeric type");
+
+  static std::random_device rd;
+  static std::mt19937 gen(rd());
+
+  if constexpr (std::is_integral<T>::value) {
+    std::uniform_int_distribution<T> dis(min, max);
+    return dis(gen);
+  } else {
+    std::uniform_real_distribution<T> dis(min, max);
+    return dis(gen);
+  }
+}
+
+static std::vector<std::string> split(const std::string& str, char delimiter=' ') {
+  std::vector<std::string> tokens;
+  std::string token;
+  std::istringstream tokenStream(str);
+  
+  while (std::getline(tokenStream, token, delimiter)) {
+    tokens.push_back(token);
+  }
+  
+  return tokens;
 }
 }  // namespace spt
 
