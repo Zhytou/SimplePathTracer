949bd82580d03f696b022d671793aaf38eaf9528
Zhytou <zhoytou@gmail.com>
2026-01-11 22:45:02 +0800

    update light 1、修改sample函数，主要是pdflight的计算。 2、增加pdf函数，根据采样光线计算pdf。

--- include/Light.hpp
+++ include/Light.hpp
@@ -38,52 +38,49 @@ namespace spt
             lights.push_back(triangle);
         }
 
-        std::vector<std::pair<Vec3<float>, float>> sampleAll(const std::shared_ptr<BVH>& scene, const Vec3<float>& p) {
-            std::vector<std::pair<Vec3<float>, float>> ret;
-            
-            for (int gidx = 0; gidx < groups.size(); gidx++) {
-                int lidx = groups[gidx][rand(groups[gidx].size() - 1)];
-                
-                Vec3<float> pp = lights[lidx]->getRandomPoint();
-                Vec3<float> dir = normalize(pp - p);
-                float pdf = 0.f;
-
-                Ray ray(p, dir);
-                HitResult res;
-                scene->hit(ray, res);
-                
-                if (!res.hit || res.material.getName() != lights[lidx]->getMaterial().getName()) {
-                    dir = Vec3<float>(0, 0, 0);
-                } else {
-                    float area = areas[gidx];
-                    pdf = 1 / area;
-                }
-
-                ret.emplace_back(dir, pdf);
-            }
-            return ret;
-        }
-
-        std::pair<Vec3<float>, float> sample(const std::shared_ptr<BVH>& scene, const Vec3<float>& p) {
+        std::tuple<Vec3<float>, Vec3<float>, float> sample(const std::shared_ptr<BVH>& scene, const Vec3<float>& p) {
             ulong gidx = rand(groups.size() - 1);
             ulong lidx = groups[gidx][rand(groups[gidx].size() - 1)];
             
+            Vec3<float> n = lights[lidx]->getNormal();
             Vec3<float> pp = lights[lidx]->getRandomPoint();
+            Vec3<float> lum(0.f, 0.f, 0.f);
             Vec3<float> dir = normalize(pp - p);
             float pdf = 0.f;
+            float cos = std::max(dot(dir, -n), 0.f);
 
             Ray ray(p, dir);
             HitResult res;
             scene->hit(ray, res);
             
-            if (!res.hit || res.material.getName() != lights[lidx]->getMaterial().getName()) {
-                dir = Vec3(0.f, 0.f, 0.f);
-            } else {
-                float area = areas[gidx];
-                pdf = 1 / area;
+            if (res.hit && res.id == lights[lidx]->getId()) {
+                float nom = res.distance * res.distance;
+                float denom = std::max(areas[gidx] * cos, 0.00001f);
+                lum = lights[lidx]->getMaterial().getEmission();
+                pdf = nom / denom;
             }
 
-            return {dir, pdf};
+            return {lum, dir, pdf};
+        }
+
+        float pdf(const std::shared_ptr<BVH>& scene, const Ray& ray) {
+            HitResult res;
+            scene->hit(ray, res);
+
+            float pdf = 0.f;
+            Material mtl = res.material;
+            Vec3<float> n = res.normal;
+            Vec3<float> dir = ray.getDirection();
+            float cos = std::max(dot(dir, -n), 0.f);
+
+            if (res.hit && mtl.isEmissive()) {
+                ulong gidx = mtlnames[mtl.getName()];
+                float nom = res.distance * res.distance;
+                float denom = std::max(areas[gidx] * cos, 0.00001f);
+                pdf = nom / denom;
+            }
+
+            return pdf;
         }
     };
 } // namespace spt
