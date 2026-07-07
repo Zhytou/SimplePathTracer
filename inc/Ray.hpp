6b0873879714fe0121790320b8fdc35ebcd3d322
Zhytou <Zhoytou@gmail.com>
2026-07-07 19:47:38 +0800

    format Ray

--- inc/Ray.hpp
+++ inc/Ray.hpp
@@ -1,25 +1,25 @@
-#ifndef SRE_RAY_HPP
-#define SRE_RAY_HPP
+#ifndef SPT_RAY_HPP
+#define SPT_RAY_HPP
 
 #include "Utils.hpp"
 
 namespace spt {
 class Ray {
- private:
-  Vec3<float> origin;
-  Vec3<float> direction;
+   public:
+    Ray()  = default;
+    ~Ray() = default;
+    Ray(const Vec3<float>& org, const Vec3<float>& dir) : m_origin(org), m_direction(normalize(dir)) {}
 
- public:
-  Ray() = default;
-  ~Ray() = default;
-  Ray(const Vec3<float> &org, const Vec3<float> &dir) : origin(org), direction(normalize(dir)) {}
+    // getter
+    Vec3<float> getOrigin() const { return m_origin; }
+    Vec3<float> getDirection() const { return m_direction; }
+    Vec3<float> getPointAt(const float& t) const { return m_origin + m_direction * t; }
 
-  // getter
-  Vec3<float> getOrigin() const { return origin; }
-  Vec3<float> getDirection() const { return direction; }
-  Vec3<float> getPointAt(const float &t) const { return origin + direction * t; }
+   private:
+    Vec3<float> m_origin;
+    Vec3<float> m_direction;
 };
 
-}  // namespace spt
+} // namespace spt
 
 #endif
