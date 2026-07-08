4cf1b0180c3715ae990dd0ad3bca66191ed6282c
Zhytou <Zhoytou@gmail.com>
2026-07-08 15:05:52 +0800

    format

--- inc/Utils.hpp
+++ inc/Utils.hpp
@@ -1,230 +1,231 @@
-#ifndef SRE_UTILS_HPP
-#define SRE_UTILS_HPP
+#ifndef SPT_UTILS_HPP
+#define SPT_UTILS_HPP
 
 #include <cassert>
 #include <cmath>
-#include <string>
-#include <vector>
 #include <ostream>
-#include <sstream>
-#include <type_traits>
 #include <random>
-
-#define PI 3.14159265358979323846f
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <vector>
 
 namespace spt {
+constexpr float EPS = 1e-5f;
+
+constexpr float PI = 3.14159265358979323846f;
 
 template <typename T>
 struct Vec2 {
-  T u, v;
+    T u, v;
 
-  Vec2() = default;
+    Vec2() = default;
 
-  ~Vec2() = default;
+    ~Vec2() = default;
 
-  Vec2(const T& a, const T& b) : u(a), v(b) {}
+    Vec2(const T& a, const T& b) : u(a), v(b) {}
 
-  Vec2<T> operator-(const Vec2<T>& other) const {
-    return Vec2<T>(u - other.u, v - other.v);
-  }
+    Vec2<T> operator-(const Vec2<T>& other) const {
+        return Vec2<T>(u - other.u, v - other.v);
+    }
 
-  Vec2<T> operator+(const Vec2<T>& other) const {
-    return Vec2<T>(u + other.u, v + other.v);
-  }
+    Vec2<T> operator+(const Vec2<T>& other) const {
+        return Vec2<T>(u + other.u, v + other.v);
+    }
 
-  template <typename K>
-  Vec2<T> operator*(const K& k) const {
-    return Vec2<T>(u * k, v * k);
-  }
-
+    template <typename K>
+    Vec2<T> operator*(const K& k) const {
+        return Vec2<T>(u * k, v * k);
+    }
 };
 
 template <typename T>
 struct Vec3 {
-  T x, y, z;
+    T x, y, z;
 
-  Vec3() = default;
+    Vec3() = default;
 
-  ~Vec3() = default;
-  
-  Vec3(const T& a, const T& b, const T& c) : x(a), y(b), z(c) {}
+    ~Vec3() = default;
 
-  Vec3(const T arr[3]) : x(arr[0]), y(arr[1]), z(arr[2]) {}
+    Vec3(const T& a, const T& b, const T& c) : x(a), y(b), z(c) {}
 
-  Vec3& operator=(const Vec3<T>& other) {
-    x = other.x;
-    y = other.y;
-    z = other.z;
-    return *this;
-  }
+    Vec3(const T arr[3]) : x(arr[0]), y(arr[1]), z(arr[2]) {}
 
-  bool operator==(const Vec3<T>& other) const {
-    return x == other.x && y == other.y && z == other.z;
-  }
-  
-  bool operator!=(const Vec3<T>& other) const {
-    return x != other.x || y != other.y || z != other.z;
-  }
-  
-  Vec3<T> operator-() const { return Vec3<T>(-x, -y, -z); }
-  
-  Vec3<T> operator-(const Vec3<T>& other) const {
-    return Vec3<T>(x - other.x, y - other.y, z - other.z);
-  }
-  
-  Vec3<T> operator+(const Vec3<T>& other) const {
-    return Vec3<T>(x + other.x, y + other.y, z + other.z);
-  }
-  
-  template <typename K>
-  Vec3<T> operator*(const K& k) const {
-    return Vec3<T>(x * k, y * k, z * k);
-  }
-  
-  Vec3<T> operator*(const Vec3<T>& other) const {
-    return Vec3<T>(x * other.x, y * other.y, z * other.z);
-  }
-  
-  template <typename K>
-  Vec3<T> operator/(const K& k) const {
-    return Vec3<T>(x / k, y / k, z / k);
-  }
-
-  Vec3<T> operator/(const Vec3<T>& other) const {
-    return Vec3<T>(x / other.x, y / other.y, z / other.z);
-  }
-  
-  Vec3<T>& operator-=(const Vec3<T>& other) {
-    x -= other.x;
-    y -= other.y;
-    z -= other.z;
-    return *this;
-  }
-  
-  Vec3<T>& operator+=(const Vec3<T>& other) {
-    x += other.x;
-    y += other.y;
-    z += other.z;
-    return *this;
-  }
-  
-  template <typename K>
-  Vec3<T>& operator*=(const K& k) {
-    x *= k;
-    y *= k;
-    z *= k;
-    return *this;
-  }
-  
-  template <typename K>
-  Vec3<T>& operator/=(const K& k) {
-    x /= k;
-    y /= k;
-    z /= k;
-    return *this;
-  }
-
-  bool operator>(const Vec3<T>& other) {
-    return x > other.x && y > other.y && z > other.z;
-  }
-
-  bool operator>=(const Vec3<T>& other) {
-    return x >= other.x && y >= other.y && z >= other.z;
-  }
-  
-  bool operator<(const Vec3<T>& other) {
-    return x < other.x && y < other.y && z < other.z;
-  }
-
-  bool operator<=(const Vec3<T>& other) {
-    return x <= other.x && y <= other.y && z <= other.z;
-  }
-
-  Vec3<T>& normalize() {
-    double d = length();
-    if (d > 0.f) {
-      x /= d;
-      y /= d;
-      z /= d;
+    Vec3& operator=(const Vec3<T>& other) {
+        x = other.x;
+        y = other.y;
+        z = other.z;
+        return *this;
     }
-    return *this;
-  }
 
-  T length() const { return ::sqrt(x * x + y * y + z * z); }
+    bool operator==(const Vec3<T>& other) const {
+        return x == other.x && y == other.y && z == other.z;
+    }
 
-  T max() const { return std::max(std::max(x, y), z); }
+    bool operator!=(const Vec3<T>& other) const {
+        return x != other.x || y != other.y || z != other.z;
+    }
 
-  T min() const { return std::min(std::min(x, y), z); }
+    Vec3<T> operator-() const { return Vec3<T>(-x, -y, -z); }
+
+    Vec3<T> operator-(const Vec3<T>& other) const {
+        return Vec3<T>(x - other.x, y - other.y, z - other.z);
+    }
+
+    Vec3<T> operator+(const Vec3<T>& other) const {
+        return Vec3<T>(x + other.x, y + other.y, z + other.z);
+    }
+
+    template <typename K>
+    Vec3<T> operator*(const K& k) const {
+        return Vec3<T>(x * k, y * k, z * k);
+    }
+
+    Vec3<T> operator*(const Vec3<T>& other) const {
+        return Vec3<T>(x * other.x, y * other.y, z * other.z);
+    }
+
+    template <typename K>
+    Vec3<T> operator/(const K& k) const {
+        return Vec3<T>(x / k, y / k, z / k);
+    }
+
+    Vec3<T> operator/(const Vec3<T>& other) const {
+        return Vec3<T>(x / other.x, y / other.y, z / other.z);
+    }
+
+    Vec3<T>& operator-=(const Vec3<T>& other) {
+        x -= other.x;
+        y -= other.y;
+        z -= other.z;
+        return *this;
+    }
+
+    Vec3<T>& operator+=(const Vec3<T>& other) {
+        x += other.x;
+        y += other.y;
+        z += other.z;
+        return *this;
+    }
+
+    template <typename K>
+    Vec3<T>& operator*=(const K& k) {
+        x *= k;
+        y *= k;
+        z *= k;
+        return *this;
+    }
+
+    template <typename K>
+    Vec3<T>& operator/=(const K& k) {
+        x /= k;
+        y /= k;
+        z /= k;
+        return *this;
+    }
+
+    bool operator>(const Vec3<T>& other) {
+        return x > other.x && y > other.y && z > other.z;
+    }
+
+    bool operator>=(const Vec3<T>& other) {
+        return x >= other.x && y >= other.y && z >= other.z;
+    }
+
+    bool operator<(const Vec3<T>& other) {
+        return x < other.x && y < other.y && z < other.z;
+    }
+
+    bool operator<=(const Vec3<T>& other) {
+        return x <= other.x && y <= other.y && z <= other.z;
+    }
+
+    Vec3<T>& normalize() {
+        double d = length();
+        if (d > 0.f) {
+            x /= d;
+            y /= d;
+            z /= d;
+        }
+        return *this;
+    }
+
+    T length() const { return ::sqrt(x * x + y * y + z * z); }
+
+    T max() const { return std::max(std::max(x, y), z); }
+
+    T min() const { return std::min(std::min(x, y), z); }
 };
 
-template<typename T>
+template <typename T>
 std::ostream& operator<<(std::ostream& os, const Vec2<T>& v) {
-  os << "Vec2(" << v.u << ", " << v.v << ")";
-  return os;
+    os << "Vec2(" << v.u << ", " << v.v << ")";
+    return os;
 }
 
-template<typename T>
+template <typename T>
 std::ostream& operator<<(std::ostream& os, const Vec3<T>& v) {
-  os << "Vec3(" << v.x << ", " << v.y << ", " << v.z << ")";
-  return os;
+    os << "Vec3(" << v.x << ", " << v.y << ", " << v.z << ")";
+    return os;
 }
 
-template<typename T>
+template <typename T>
 static Vec3<T> normalize(const Vec3<T>& v) {
-  double d = v.length();
-  if (d > 0.f) {
-    return Vec3<T>(v.x / d, v.y / d, v.z / d);
-  }
-  return v;
+    double d = v.length();
+    if (d > 0.f) {
+        return Vec3<T>(v.x / d, v.y / d, v.z / d);
+    }
+    return v;
 }
 
-template<typename T>
+template <typename T>
 static Vec3<T> cross(const Vec3<T>& v1, const Vec3<T>& v2) {
-  return Vec3<T>(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
+    return Vec3<T>(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
 }
 
-template<typename T>
+template <typename T>
 T dot(const Vec3<T>& v1, const Vec3<T>& v2) {
-  return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 }
 
-template<typename T>
+template <typename T>
 static T distance(const Vec3<T>& v1, const Vec3<T>& v2) {
-  return ::sqrt((v1.x - v2.x) * (v1.x - v2.x) + (v1.y - v2.y) * (v1.y - v2.y) + (v1.z - v2.z) * (v1.z - v2.z));
+    return ::sqrt((v1.x - v2.x) * (v1.x - v2.x) + (v1.y - v2.y) * (v1.y - v2.y) + (v1.z - v2.z) * (v1.z - v2.z));
 }
 
-template<typename T, typename K>
+template <typename T, typename K>
 static Vec3<T> pow(const Vec3<T>& v, const K& k) {
-  return Vec3<T>(::pow(v.x, k), ::pow(v.y, k), ::pow(v.z, k));
+    return Vec3<T>(::pow(v.x, k), ::pow(v.y, k), ::pow(v.z, k));
 }
 
-template<typename T>
+template <typename T>
 T rand(T max, T min = 0) {
-  static_assert(std::is_arithmetic<T>::value, "T must be numeric type");
+    static_assert(std::is_arithmetic<T>::value, "T must be numeric type");
 
-  static std::random_device rd;
-  static std::mt19937 gen(rd());
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
 
-  if constexpr (std::is_integral<T>::value) {
-    std::uniform_int_distribution<T> dis(min, max);
-    return dis(gen);
-  } else {
-    std::uniform_real_distribution<T> dis(min, max);
-    return dis(gen);
-  }
+    if constexpr (std::is_integral<T>::value) {
+        std::uniform_int_distribution<T> dis(min, max);
+        return dis(gen);
+    } else {
+        std::uniform_real_distribution<T> dis(min, max);
+        return dis(gen);
+    }
 }
 
-static std::vector<std::string> split(const std::string& str, char delimiter=' ') {
-  std::vector<std::string> tokens;
-  std::string token;
-  std::istringstream tokenStream(str);
-  
-  while (std::getline(tokenStream, token, delimiter)) {
-    tokens.push_back(token);
-  }
-  
-  return tokens;
+static std::vector<std::string> split(const std::string& str, char delimiter = ' ') {
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream tokenStream(str);
+
+    while (std::getline(tokenStream, token, delimiter)) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
 }
-}  // namespace spt
+
+} // namespace spt
 
 #endif
