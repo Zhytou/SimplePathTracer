d9a75d39964c4e9f3edba1fe30e2aacb098a1d1c
Zhytou <Zhoytou@gmail.com>
2026-07-10 03:19:11 +0800

    add comment for BVH::constructBVH

--- inc/BVH.hpp
+++ inc/BVH.hpp
@@ -16,6 +16,12 @@ class BVH : public Hittable {
    public:
     BVH(int count, bool leaf) : Hittable(-1), m_count(count), m_leaf(leaf) {}
 
+    // Construct bounding volume hierarchy structure with [beg, end) triangles
+    // @param triangles: vector of shared pointers to triangles
+    // @param beg: begin index of the triangles to construct the bvh
+    // @param end: end index of the triangles to construct the bvh
+    // @param cnt: minimum number of triangles in a leaf node
+    // @return: shared pointer to the root node of the bvh
     static std::shared_ptr<BVH> constructBVH(std::vector<std::shared_ptr<Triangle>>& triangles, int beg, int end, int cnt);
     static void sortObjects(std::vector<std::shared_ptr<Triangle>>& triangles, int beg, int end, int axis);
     static float computeSAH(const AABB& parent, const AABB& left, const AABB& right, int cnt1, int cnt2);
