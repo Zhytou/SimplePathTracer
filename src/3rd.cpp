1a7682a3e9f02b7f4e1ff4f7953c7bce4e7cb440
Zhytou <Zhoytou@gmail.com>
2026-07-10 16:06:10 +0800

    add 3rd.lib

---
+++ src/3rd.cpp
@@ -0,0 +1,8 @@
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STBI_FAILURE_USERMSG
+#include <stb_image.h>
+#include <stb_image_write.h>
