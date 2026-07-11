8dee29ce5d70df85b86a5e8d42fbf7ea0093823b
Zhytou <Zhoytou@gmail.com>
2026-07-11 18:07:41 +0800

    update Ray setter

--- inc/Ray.hpp
+++ inc/Ray.hpp
@@ -10,10 +10,11 @@ class Ray {
     ~Ray() = default;
     Ray(const Vec3<float>& org, const Vec3<float>& dir) : m_origin(org), m_direction(normalize(dir)) {}
 
-    // getter
     Vec3<float> getOrigin() const { return m_origin; }
     Vec3<float> getDirection() const { return m_direction; }
     Vec3<float> getPointAt(const float& t) const { return m_origin + m_direction * t; }
+    void setOrigin(const Vec3<float>& org) { m_origin = org; }
+    void setDirection(const Vec3<float>& dir) { m_direction = normalize(dir); }
 
    private:
     Vec3<float> m_origin;
