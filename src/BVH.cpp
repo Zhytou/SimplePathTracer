7f80d9256c57eb0597c033d86e4ed07d1943de25
Zhytou <Zhoytou@gmail.com>
2026-07-11 18:05:59 +0800

    update BVH::constructBVH(use Renderable as BVH leaf node children)

--- src/BVH.cpp
+++ src/BVH.cpp
@@ -2,22 +2,22 @@
 
 namespace spt {
 
-std::shared_ptr<BVH> BVH::constructBVH(std::vector<std::shared_ptr<Triangle>>& triangles, int beg, int end, int cnt) {
+std::shared_ptr<BVH> BVH::constructBVH(std::vector<std::shared_ptr<Renderable>>& objects, int beg, int end, int cnt) {
     int num     = end - beg;
     auto bvh    = std::make_shared<BVH>(num, false);
-    bvh->m_aabb = mergeAABBs(triangles, beg, end);
+    bvh->m_aabb = mergeAABBs(objects, beg, end);
 
-    // 1. Construct leaf node if the number of triangles in the current bvh is less than or equal to cnt
+    // 1. Construct leaf node if the number of objects in the current bvh is less than or equal to cnt
     if (num <= cnt) {
         bvh->m_leaf = true;
-        bvh->m_children.assign(triangles.begin() + beg, triangles.begin() + end);
+        bvh->m_children.assign(objects.begin() + beg, objects.begin() + end);
         return bvh;
     }
 
     // 2. Find the longest axis to split the triangles by
     auto delta = bvh->m_aabb.getDelta();
     int axis   = delta.x >= max(delta) ? 0 : (delta.y >= max(delta) ? 1 : 2);
-    sortObjects(triangles, beg, end, axis);
+    sortObjects(objects, beg, end, axis);
 
     // 3. Iterate to find best split
     std::pair<int, float> split = {-1, INFINITY};
@@ -25,8 +25,8 @@ std::shared_ptr<BVH> BVH::constructBVH(std::vector<std::shared_ptr<Triangle>>& t
         int lcnt = idx - beg;
         int rcnt = num - lcnt;
 
-        auto left  = mergeAABBs(triangles, beg, idx);
-        auto right = mergeAABBs(triangles, idx, end);
+        auto left  = mergeAABBs(objects, beg, idx);
+        auto right = mergeAABBs(objects, idx, end);
         float cost = computeSAH(bvh->m_aabb, left, right, lcnt, rcnt);
 
         if (cost < split.second) {
@@ -37,22 +37,22 @@ std::shared_ptr<BVH> BVH::constructBVH(std::vector<std::shared_ptr<Triangle>>& t
     // 4. Make it a leaf node if splitting is more expensive than keeping it as a leaf
     if (split.first == -1 || split.second >= num * 1.1f) {
         bvh->m_leaf = true;
-        bvh->m_children.assign(triangles.begin() + beg, triangles.begin() + end);
+        bvh->m_children.assign(objects.begin() + beg, objects.begin() + end);
         return bvh;
     }
 
     // construct sub bvh
     bvh->m_children.assign(2, nullptr);
-    bvh->m_children[0] = constructBVH(triangles, beg, split.first, cnt);
-    bvh->m_children[1] = constructBVH(triangles, split.first, end, cnt);
+    bvh->m_children[0] = constructBVH(objects, beg, split.first, cnt);
+    bvh->m_children[1] = constructBVH(objects, split.first, end, cnt);
 
     return bvh;
 }
 
-void BVH::sortObjects(std::vector<std::shared_ptr<Triangle>>& triangles, int beg, int end, int axis) {
-    std::stable_sort(triangles.begin() + beg, triangles.begin() + end, [axis](std::shared_ptr<Triangle> triangle1, std::shared_ptr<Triangle> triangle2) {
-        auto xyz1 = triangle1->wrap().getCenter();
-        auto xyz2 = triangle2->wrap().getCenter();
+void BVH::sortObjects(std::vector<std::shared_ptr<Renderable>>& objects, int beg, int end, int axis) {
+    std::stable_sort(objects.begin() + beg, objects.begin() + end, [axis](std::shared_ptr<Renderable> object1, std::shared_ptr<Renderable> object2) {
+        auto xyz1 = object1->wrap().getCenter();
+        auto xyz2 = object2->wrap().getCenter();
 
         if (axis == 0) {
             return xyz1.x < xyz2.x;
@@ -64,10 +64,10 @@ void BVH::sortObjects(std::vector<std::shared_ptr<Triangle>>& triangles, int beg
     });
 }
 
-AABB BVH::mergeAABBs(std::vector<std::shared_ptr<Triangle>>& triangles, int beg, int end) {
+AABB BVH::mergeAABBs(std::vector<std::shared_ptr<Renderable>>& objects, int beg, int end) {
     AABB aabb;
     for (int i = beg; i < end; i++) {
-        auto aabbi = triangles[i]->wrap();
+        auto aabbi = objects[i]->wrap();
         aabb.merge(aabbi);
     }
     return aabb;
@@ -84,16 +84,16 @@ bool BVH::hit(const Ray& ray, float tmin, float tmax, HitRecord& rec) const {
 
     // 2. Check triangle intersection, if leaf node
     if (m_leaf) {
-        bool ishit = false;
+        bool hit = false;
         for (auto child : m_children) {
             HitRecord crec;
             if (child->hit(ray, tmin, tmax, crec)) {
-                ishit = true;
-                tmax  = std::min(crec.distance, tmax);
-                rec   = crec;
+                hit  = true;
+                tmax = std::min(crec.distance, tmax);
+                rec  = crec;
             }
         }
-        return ishit;
+        return hit;
     }
 
     // 3. Check sub bvh intersection, if not leaf node
