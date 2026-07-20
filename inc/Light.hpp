94506f53921c3b00eb34e4a406b48f09596eedf9
Zhytou <Zhoytou@gmail.com>
2026-07-20 15:10:40 +0800

    add m_id for Light Class

--- inc/Light.hpp
+++ inc/Light.hpp
@@ -13,11 +13,13 @@ namespace spt {
 
 class Light {
    public:
-    Light(const Vec3<float>& color) : m_color(color) {}
+    Light(int id, const Vec3<float>& color) : m_id(id), m_color(color) {}
     virtual ~Light() {}
 
     virtual bool isDelta() const = 0;
-    virtual int getID() const    = 0;
+    int getID() const { return m_id; }
+    virtual int getObjectID() const { return -1; }
+    virtual float getObjectArea() const { return 0.f; }
     Vec3<float> getColor() const { return m_color; }
 
     virtual Vec3<float> sample(const Vec3<float>& p) const                          = 0;
@@ -25,15 +27,17 @@ class Light {
 
    private:
     Vec3<float> m_color = Vec3<float>(0.f);
+    int m_id            = -1;
 };
 
 class AreaLight : public Light {
    public:
-    AreaLight(const Vec3<float>& color, std::shared_ptr<Renderable> object) : Light(color), m_object(object) {}
+    AreaLight(int id, const Vec3<float>& color, std::shared_ptr<Renderable> object) : Light(id, color), m_object(object) {}
     ~AreaLight() {}
 
     virtual bool isDelta() const override { return false; }
-    virtual int getID() const override { return m_object ? m_object->getID() : -1; }
+    virtual int getObjectID() const override { return m_object ? m_object->getID() : -1; }
+    virtual float getObjectArea() const override { return m_object ? m_object->getArea() : 0.f; }
     virtual Vec3<float> sample(const Vec3<float>& p) const override;
     virtual float pdf(const Vec3<float>& wo, const Vec3<float>& n, float dis) const override;
 
@@ -43,11 +47,10 @@ class AreaLight : public Light {
 
 class PointLight : public Light {
    public:
-    PointLight(const Vec3<float>& color, const Vec3<float>& position) : Light(color), m_position(position) {}
+    PointLight(int id, const Vec3<float>& color, const Vec3<float>& position) : Light(id, color), m_position(position) {}
     ~PointLight() {}
 
     virtual bool isDelta() const override { return true; }
-    virtual int getID() const override { return -1; }
     virtual Vec3<float> sample(const Vec3<float>& p) const override { return normalize(m_position - p); }
     virtual float pdf(const Vec3<float>& wo, const Vec3<float>& n, float dis) const override { return INFINITY; }
 
@@ -57,11 +60,10 @@ class PointLight : public Light {
 
 class DirectionalLight : public Light {
    public:
-    DirectionalLight(const Vec3<float>& color, const Vec3<float>& direction) : Light(color), m_direction(normalize(direction)) {}
+    DirectionalLight(int id, const Vec3<float>& color, const Vec3<float>& direction) : Light(id, color), m_direction(normalize(direction)) {}
     ~DirectionalLight() {}
 
     virtual bool isDelta() const override { return true; }
-    virtual int getID() const override { return -1; }
     virtual Vec3<float> sample(const Vec3<float>& p) const override { return -m_direction; }
     virtual float pdf(const Vec3<float>& wo, const Vec3<float>& n, float dis) const override { return INFINITY; }
 
