1591b592d7d8f4961cd76d4b43b672c4e8368995
Zhytou <Zhoytou@gmail.com>
2026-07-21 01:03:08 +0800

    add concept constrain and left multiply for Vec

--- inc/Vec.hpp
+++ inc/Vec.hpp
@@ -3,13 +3,17 @@
 
 #include <cassert>
 #include <cmath>
+#include <concepts>
 #include <sstream>
 #include <string>
 #include <type_traits>
 
 namespace spt {
 
-template <typename T, size_t N>
+template <typename T>
+concept arithmetic = std::integral<T> || std::floating_point<T>;
+
+template <arithmetic T, size_t N>
 struct VecBase {
     T data[N];
 
@@ -28,7 +32,7 @@ struct VecBase {
     }
 };
 
-template <typename T>
+template <arithmetic T>
 struct VecBase<T, 2> {
     union {
         T data[2];
@@ -61,7 +65,7 @@ struct VecBase<T, 2> {
     }
 };
 
-template <typename T>
+template <arithmetic T>
 struct VecBase<T, 3> {
     union {
         T data[3];
@@ -97,7 +101,7 @@ struct VecBase<T, 3> {
     }
 };
 
-template <typename T, size_t N>
+template <arithmetic T, size_t N>
 struct Vec : public VecBase<T, N> {
     // Inherit constructors. The subclass directly reuses all of the base class's constructors and avoid manually writing forwarding constructors: Vec(const T arr[N]) : VecBase(arr) {}
     using VecBase<T, N>::VecBase;
@@ -124,7 +128,7 @@ struct Vec : public VecBase<T, N> {
         return *this;
     }
 
-    template <typename K>
+    template <arithmetic K>
     Vec<T, N>& operator*=(const K& k) {
         for (int i = 0; i < N; i++) { data[i] *= k; }
         return *this;
@@ -141,18 +145,18 @@ struct Vec : public VecBase<T, N> {
     }
 };
 
-template <typename T>
+template <arithmetic T>
 using Vec2 = Vec<T, 2>;
-template <typename T>
+template <arithmetic T>
 using Vec3 = Vec<T, 3>;
-template <typename T>
+template <arithmetic T>
 using Vec4 = Vec<T, 4>;
 
 using Vec2f = Vec2<float>;
 using Vec3f = Vec3<float>;
 using Vec4f = Vec4<float>;
 
-template <typename T, size_t N>
+template <arithmetic T, size_t N>
 bool operator==(const Vec<T, N>& v1, const Vec<T, N>& v2) {
     for (int i = 0; i < N; i++) {
         if (v1.data[i] != v2.data[i]) { return false; }
@@ -160,7 +164,7 @@ bool operator==(const Vec<T, N>& v1, const Vec<T, N>& v2) {
     return true;
 }
 
-template <typename T, size_t N>
+template <arithmetic T, size_t N>
 bool operator!=(const Vec<T, N>& v1, const Vec<T, N>& v2) {
     for (int i = 0; i < N; i++) {
         if (v1.data[i] == v2.data[i]) { return false; }
@@ -168,56 +172,63 @@ bool operator!=(const Vec<T, N>& v1, const Vec<T, N>& v2) {
     return true;
 }
 
-template <typename T, size_t N>
+template <arithmetic T, size_t N>
 Vec<T, N> operator+(const Vec<T, N>& v1, const Vec<T, N>& v2) {
     Vec<T, N> v;
     for (int i = 0; i < N; i++) { v.data[i] = v1.data[i] + v2.data[i]; }
     return v;
 }
 
-template <typename T, size_t N>
+template <arithmetic T, size_t N>
 Vec<T, N> operator-(const Vec<T, N>& v) {
     Vec<T, N> vn; // negative vector
     for (size_t i = 0; i < N; ++i) { vn.data[i] = -v.data[i]; }
     return vn;
 }
 
-template <typename T, size_t N>
+template <arithmetic T, size_t N>
 Vec<T, N> operator-(const Vec<T, N>& v1, const Vec<T, N>& v2) {
     Vec<T, N> v;
     for (int i = 0; i < N; i++) { v.data[i] = v1.data[i] - v2.data[i]; }
     return v;
 }
 
-template <typename T, typename K, size_t N>
+template <arithmetic T, arithmetic K, size_t N>
 Vec<T, N> operator*(const Vec<T, N>& v, const K& k) {
     Vec<T, N> vm; // multiply vector
     for (int i = 0; i < N; i++) { vm.data[i] = v.data[i] * k; }
     return vm;
 }
 
-template <typename T, size_t N>
+template <arithmetic T, arithmetic K, size_t N>
+Vec<T, N> operator*(const K& k, const Vec<T, N>& v) {
+    Vec<T, N> vm; // multiply vector
+    for (int i = 0; i < N; i++) { vm.data[i] = v.data[i] * k; }
+    return vm;
+}
+
+template <arithmetic T, size_t N>
 Vec<T, N> operator*(const Vec<T, N>& v1, const Vec<T, N>& v2) {
     Vec<T, N> v;
     for (int i = 0; i < N; i++) { v.data[i] = v1.data[i] * v2.data[i]; }
     return v;
 }
 
-template <typename T, typename K, size_t N>
+template <arithmetic T, arithmetic K, size_t N>
 Vec<T, N> operator/(const Vec<T, N>& v, const K& k) {
     Vec<T, N> vd; // divide vector
     for (int i = 0; i < N; i++) { vd.data[i] = v.data[i] / k; }
     return vd;
 }
 
-template <typename T, size_t N>
+template <arithmetic T, size_t N>
 Vec<T, N> operator/(const Vec<T, N>& v1, const Vec<T, N>& v2) {
     Vec<T, N> v;
     for (int i = 0; i < N; i++) { v.data[i] = v1.data[i] / v2.data[i]; }
     return v;
 }
 
-template <typename T, size_t N>
+template <arithmetic T, size_t N>
 std::ostream& operator<<(std::ostream& os, const Vec<T, N>& v) {
     std::stringstream ss;
     ss << "Vec" << N << "(";
@@ -230,14 +241,14 @@ std::ostream& operator<<(std::ostream& os, const Vec<T, N>& v) {
     return os;
 }
 
-template <typename T, size_t N>
+template <arithmetic T, size_t N>
 T length(const Vec<T, N>& v) {
     T d = 0;
     for (int i = 0; i < N; i++) { d += v.data[i] * v.data[i]; }
     return std::sqrt(d);
 }
 
-template <typename T, size_t N>
+template <arithmetic T, size_t N>
 Vec<T, N> normalize(const Vec<T, N>& v) {
     Vec<T, N> vn = v; // normalized vector
     T d          = length(v);
@@ -247,7 +258,7 @@ Vec<T, N> normalize(const Vec<T, N>& v) {
     return vn;
 }
 
-template <typename T, size_t N>
+template <arithmetic T, size_t N>
 Vec<T, N> cross(const Vec<T, N>& v1, const Vec<T, N>& v2) {
     Vec<T, N> v;
     static_assert(N == 3, "cross product only defined for 3-dimensional Vec");
@@ -257,35 +268,35 @@ Vec<T, N> cross(const Vec<T, N>& v1, const Vec<T, N>& v2) {
     return v;
 }
 
-template <typename T, size_t N>
+template <arithmetic T, size_t N>
 T dot(const Vec<T, N>& v1, const Vec<T, N>& v2) {
     T d = 0.f;
     for (int i = 0; i < N; i++) { d += v1.data[i] * v2.data[i]; }
     return d;
 }
 
-template <typename T, size_t N>
+template <arithmetic T, size_t N>
 T distance(const Vec<T, N>& v1, const Vec<T, N>& v2) {
     T d = 0.0;
     for (int i = 0; i < N; i++) { d += (v1.data[i] - v2.data[i]) * (v1.data[i] - v2.data[i]); }
     return ::sqrt(d);
 }
 
-template <typename T, typename K, size_t N>
+template <arithmetic T, arithmetic K, size_t N>
 Vec<T, N> pow(const Vec<T, N>& v, const K& k) {
     Vec<T, N> vp; // powered vector
     for (int i = 0; i < N; i++) { vp.data[i] = std::pow(v.data[i], k); }
     return vp;
 }
 
-template <typename T, size_t N>
+template <arithmetic T, size_t N>
 Vec<T, N> clamp(const Vec<T, N>& v, const T& t1, const T& t2) {
     Vec<T, N> vc; // clamped vector
     for (int i = 0; i < N; i++) { vc.data[i] = std::min(std::max(v.data[i], t1), t2); }
     return vc;
 }
 
-template <typename T, size_t N>
+template <arithmetic T, size_t N>
 T max(const Vec<T, N>& v) {
     T t = v.data[0];
     for (int i = 1; i < N; i++) {
@@ -294,7 +305,7 @@ T max(const Vec<T, N>& v) {
     return t;
 }
 
-template <typename T, size_t N>
+template <arithmetic T, size_t N>
 Vec<T, N> max(const Vec<T, N>& v1, const Vec<T, N>& v2) {
     Vec<T, N> v;
     for (int i = 0; i < N; i++) {
@@ -303,7 +314,7 @@ Vec<T, N> max(const Vec<T, N>& v1, const Vec<T, N>& v2) {
     return v;
 }
 
-template <typename T, size_t N>
+template <arithmetic T, size_t N>
 T min(const Vec<T, N>& v) {
     T t = v.data[0];
     for (int i = 1; i < N; i++) {
@@ -312,7 +323,7 @@ T min(const Vec<T, N>& v) {
     return t;
 }
 
-template <typename T, size_t N>
+template <arithmetic T, size_t N>
 Vec<T, N> min(const Vec<T, N>& v1, const Vec<T, N>& v2) {
     Vec<T, N> v;
     for (int i = 0; i < N; i++) {
