06c4e118aafb5e324d210773be7b35636fde794c
Zhytou <Zhoytou@gmail.com>
2026-07-29 00:39:51 +0800

    update Shape

--- inc/Sphere.hpp
+++ inc/Sphere.hpp
@@ -1,28 +1,23 @@
 #ifndef SPT_SPHERE_HPP
 #define SPT_SPHERE_HPP
 
-#include "Material.hpp"
-#include "Renderable.hpp"
-
-#include <array>
-#include <iostream>
-#include <memory>
+#include "Shape.hpp"
 
 namespace spt {
 
-class Sphere : public Renderable {
+class Sphere : public Shape {
    public:
-    Sphere(int id) : Renderable(id) {}
+    Sphere(int id) : Shape(id) {}
     ~Sphere() {}
 
     float getArea() const override { return 4 * PI * m_radius * m_radius; }
-    Vec3<float> getRandomPoint() const override;
     Vec2<float> getTexCoord(const Vec3<float>& p) const;
     void setCenter(const Vec3<float>& center) { m_center = center; }
     void setRadius(float radius) { m_radius = radius; }
 
     virtual bool hit(const Ray& ray, float tmin, float tmax, HitRecord& rec) const override;
     virtual AABB wrap() const override;
+    virtual Vec3<float> sample() const override;
 
    private:
     Vec3<float> m_center;
--- inc/Triangle.hpp
+++ inc/Triangle.hpp
@@ -1,8 +1,7 @@
 #ifndef SPT_TRIANGLE_HPP
 #define SPT_TRIANGLE_HPP
 
-#include "Material.hpp"
-#include "Renderable.hpp"
+#include "Shape.hpp"
 
 #include <array>
 #include <iostream>
@@ -10,19 +9,19 @@
 
 namespace spt {
 
-class Triangle : public Renderable {
+class Triangle : public Shape {
    public:
-    Triangle(int id) : Renderable(id) {}
+    Triangle(int id) : Shape(id) {}
     ~Triangle() {}
 
     float getArea() const override { return length(cross(m_vertex[2] - m_vertex[0], m_vertex[1] - m_vertex[0])) / 2; }
-    Vec3<float> getRandomPoint() const override;
     void setVertex(const std::array<Vec3<float>, 3>& v) { m_vertex = v; }
     void setTexCoord(const std::array<Vec2<float>, 3>& uv) { m_texcoord = uv; }
     void setNormal(const Vec3<float>& n) { m_normal = n; }
 
     virtual bool hit(const Ray& ray, float tmin, float tmax, HitRecord& rec) const override;
     virtual AABB wrap() const override;
+    Vec3<float> sample() const override;
 
    private:
     std::array<Vec3<float>, 3> m_vertex;
