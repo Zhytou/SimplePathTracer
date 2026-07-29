58b451881f2480439ff65f07dbaaaa59017f9092
Zhytou <Zhoytou@gmail.com>
2026-07-29 16:02:50 +0800

    refactor AABB

--- inc/AABB.hpp
+++ inc/AABB.hpp
@@ -11,28 +11,20 @@
 
 namespace spt {
 
-class AABB {
+struct AABB {
    public:
-    AABB()  = default;
-    ~AABB() = default;
-    AABB(const Vec3<float>& xyz1, const Vec3<float>& xyz2) : m_xyz1(xyz1), m_xyz2(xyz2) {}
+    AABB(const Vec3<float>& xyz1 = Vec3<float>{INFINITY}, const Vec3<float>& xyz2 = Vec3<float>{-INFINITY}) : m_xyz1(xyz1), m_xyz2(xyz2) {}
 
     bool intersect(const Ray& ray, float tmin, float tmax) const;
     AABB& merge(const AABB& other);
 
-    Vec3<float> getCenter() const { return (m_xyz1 + m_xyz2) / 2.f; }
-    Vec3<float> getDelta() const { return m_xyz2 - m_xyz1; }
-    float getArea() const {
+    Vec3<float> center() const { return (m_xyz1 + m_xyz2) / 2.f; }
+    Vec3<float> extent() const { return m_xyz2 - m_xyz1; }
+    float area() const {
         Vec3<float> xyz = m_xyz2 - m_xyz1;
         return xyz.x * xyz.y + xyz.x * xyz.z + xyz.y * xyz.z;
     }
-    std::pair<Vec3<float>, Vec3<float>> getBounds() const { return {m_xyz1, m_xyz2}; }
-    void setBounds(const Vec3<float>& xyz1, const Vec3<float>& xyz2) {
-        m_xyz1 = xyz1;
-        m_xyz2 = xyz2;
-    }
 
-   protected:
     Vec3<float> m_xyz1 = Vec3<float>{INFINITY};
     Vec3<float> m_xyz2 = Vec3<float>{-INFINITY};
 };
