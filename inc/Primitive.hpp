6d0f2b0dee63d23122a4ad3f022268e271b1106e
Zhytou <Zhoytou@gmail.com>
2026-08-01 03:13:36 +0800

    support Primitive translate/rotate/scale

--- inc/Primitive.hpp
+++ inc/Primitive.hpp
@@ -5,7 +5,6 @@
 #include "HitRecord.hpp"
 #include "Material.hpp"
 #include "Shape.hpp"
-#include "Transform.hpp"
 
 namespace spt {
 
@@ -13,15 +12,30 @@ class Primitive {
    public:
     Primitive()  = default;
     ~Primitive() = default;
-    Primitive(int id, std::shared_ptr<Shape> shape, std::shared_ptr<Material> material) : m_id(id), m_shape(shape), m_material(material) {}
+    Primitive(int id, std::shared_ptr<Shape> shape, std::shared_ptr<Material> material, const Mat4x4<float>& transform) : m_id(id), m_shape(shape), m_material(material) { setTransform(transform); }
 
     int getID() const { return m_id; }
     std::shared_ptr<Shape> getShape() const { return m_shape; }
     std::shared_ptr<Material> getMaterial() const { return m_material; }
     std::shared_ptr<AreaEmitter> getEmitter() const { return m_emitter; }
+    const Mat4x4<float>& getTransform() const { return m_transform; }
+    const Mat4x4<float>& getInvTransform() const { return m_inv_transform; }
+
+    void setID(int id) { m_id = id; }
     void setShape(std::shared_ptr<Shape> shape) { m_shape = shape; }
     void setMaterial(std::shared_ptr<Material> material) { m_material = material; }
     void setEmitter(std::shared_ptr<AreaEmitter> emitter) { m_emitter = emitter; }
+    void setTransform(const Mat4x4<float>& transform) {
+        m_is_identity   = transform == Mat4x4<float>::eye();
+        m_transform     = transform;
+        m_inv_transform = inv_affine(transform);
+        m_n_transform   = {
+            {transform[0][0], transform[0][1], transform[0][2]},
+            {transform[1][0], transform[1][1], transform[1][2]},
+            {transform[2][0], transform[2][1], transform[2][2]},
+        };
+        m_n_transform = transpose(inv(m_n_transform));
+    }
 
     bool hit(const Ray& ray, float tmin, float tmax, HitRecord& rec) const;
     AABB wrap() const;
@@ -34,7 +48,10 @@ class Primitive {
     std::shared_ptr<Material> m_material   = nullptr;
     std::shared_ptr<AreaEmitter> m_emitter = nullptr;
 
-    Transform m_transform;
+    bool m_is_identity            = true; // whether the transform matrix is identity
+    Mat4x4<float> m_transform     = Mat4x4<float>::eye();
+    Mat4x4<float> m_inv_transform = Mat4x4<float>::eye();
+    Mat3x3<float> m_n_transform   = Mat3x3<float>::eye(); // normal transform matrix
 };
 
 } // namespace spt
--- src/Primitive.cpp
+++ src/Primitive.cpp
@@ -3,21 +3,86 @@
 namespace spt {
 
 bool Primitive::hit(const Ray& ray, float tmin, float tmax, HitRecord& rec) const {
-    // TODO: add world space transform
-    bool hit = m_shape->hit(ray, tmin, tmax, rec);
-    rec.id   = hit ? getID() : -1;
+    // 0. Fall back to default hit test if transform matrix is identity
+    if (m_is_identity) {
+        bool hit = m_shape->hit(ray, tmin, tmax, rec);
+        rec.id   = hit ? getID() : -1;
+        return hit;
+    }
+
+    // 1. Convert ray to local space
+    Vec3<float> org_world  = ray.getOrigin();
+    Vec3<float> dir_world  = ray.getDirection(); // normalized direction vector
+    Vec4<float> org_local4 = m_inv_transform * Vec4<float>(org_world.x, org_world.y, org_world.z, 1.0);
+    Vec4<float> dir_local4 = m_inv_transform * Vec4<float>(dir_world.x, dir_world.y, dir_world.z, 0.0);
+    Vec3<float> org_local  = Vec3<float>(org_local4.x, org_local4.y, org_local4.z);
+    Vec3<float> dir_local  = Vec3<float>(dir_local4.x, dir_local4.y, dir_local4.z);
+
+    // 2. Calculate the scale factor between distance in world space and distance in local space
+    float scale                = length(dir_local);
+    Vec3<float> dir_local_norm = dir_local / scale;
+
+    // 2. Do hit test in local space
+    Ray ray_local(org_local, dir_local_norm);
+    bool hit = m_shape->hit(ray_local, tmin * scale, tmax * scale, rec);
+
+    // 3. Convert hit info into world space
+    if (hit) {
+        Vec4<float> point_world4 = m_transform * Vec4<float>(rec.point.x, rec.point.y, rec.point.z, 1.0);
+
+        rec.id       = getID();
+        rec.distance = rec.distance / scale;
+        rec.point    = Vec3<float>(point_world4.x, point_world4.y, point_world4.z);
+        rec.normal   = normalize(m_n_transform * rec.normal);
+    }
 
     return hit;
 }
 
 AABB Primitive::wrap() const {
-    // TODO: add world space transform
-    return m_shape->wrap();
+    // 0. Fall back to default bounding box if transform matrix is identity
+    if (m_is_identity) {
+        return m_shape->wrap();
+    }
+
+    // 1. Get local space bounding box
+    AABB aabb_local        = m_shape->wrap();
+    Vec3<float> xyz1_local = aabb_local.getXYZ1();
+    Vec3<float> xyz2_local = aabb_local.getXYZ2();
+
+    // 2. Generate eight corner vertices of local AABB
+    std::array<Vec4<float>, 8> xyzs_local4 = {
+        Vec4<float>(xyz1_local.x, xyz1_local.y, xyz1_local.z, 1.0),
+        Vec4<float>(xyz2_local.x, xyz1_local.y, xyz1_local.z, 1.0),
+        Vec4<float>(xyz1_local.x, xyz2_local.y, xyz1_local.z, 1.0),
+        Vec4<float>(xyz1_local.x, xyz1_local.y, xyz2_local.z, 1.0),
+
+        Vec4<float>(xyz2_local.x, xyz2_local.y, xyz2_local.z, 1.0),
+        Vec4<float>(xyz1_local.x, xyz2_local.y, xyz2_local.z, 1.0),
+        Vec4<float>(xyz2_local.x, xyz1_local.y, xyz2_local.z, 1.0),
+        Vec4<float>(xyz2_local.x, xyz2_local.y, xyz1_local.z, 1.0),
+    };
+
+    // 3. Convert all vertices to world space and compute new AABB bounds
+    Vec4<float> xyz1_world4(INFINITY), xyz2_world4(-INFINITY);
+    for (auto& xyz_local4 : xyzs_local4) {
+        auto xyz_world4 = m_transform * xyz_local4;
+        xyz1_world4     = min(xyz1_world4, xyz_world4);
+        xyz2_world4     = max(xyz2_world4, xyz_world4);
+    }
+
+    AABB aabb_world = AABB({xyz1_world4.x, xyz1_world4.y, xyz1_world4.z}, {xyz2_world4.x, xyz2_world4.y, xyz2_world4.z});
+    return aabb_world;
 }
 
 Vec3<float> Primitive::sample() const {
-    // TODO: add world space transform
-    return m_shape->sample();
+    if (m_is_identity) {
+        return m_shape->sample();
+    }
+
+    auto point_local = m_shape->sample();
+    auto point_world = m_transform * Vec4<float>(point_local.x, point_local.y, point_local.z, 1.0);
+    return {point_world.x, point_world.y, point_world.z};
 }
 
 } // namespace spt
