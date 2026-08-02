6aadc9ce480c59a773aed1b89f0ba191f1600511
Zhytou <Zhoytou@gmail.com>
2026-08-02 23:32:42 +0800

    refactor Ray class 1、add tmin and tmax：使用mutable修饰二者，方便在BVH中更新tmax尽快得出碰撞位置； 2、add medium：增加介质类对象，为后续介质stack以及类似红酒的材质渲染提供基础。

--- inc/Ray.hpp
+++ inc/Ray.hpp
@@ -1,25 +1,38 @@
 #ifndef SPT_RAY_HPP
 #define SPT_RAY_HPP
 
+#include "Medium.hpp"
 #include "Utils.hpp"
 
 namespace spt {
+
 class Ray {
    public:
-    Ray()  = default;
-    ~Ray() = default;
-    Ray(const Vec3<float>& org, const Vec3<float>& dir) : m_origin(org), m_direction(normalize(dir)) {}
+    Ray(const Vec3<float>& org, const Vec3<float>& dir, float tmin = 0.0f, float tmax = INFINITY) : m_origin(org), m_direction(normalize(dir)), m_tmin(tmin), m_tmax(tmax) {}
 
     Vec3<float> getOrigin() const { return m_origin; }
     Vec3<float> getDirection() const { return m_direction; }
+    float getTMin() const { return m_tmin; }
+    float getTMax() const { return m_tmax; }
+    std::shared_ptr<Medium> getMedium() const { return !m_medium.expired() ? m_medium.lock() : nullptr; }
     void setOrigin(const Vec3<float>& org) { m_origin = org; }
     void setDirection(const Vec3<float>& dir) { m_direction = normalize(dir); }
+    void setTMin(float tmin) const { m_tmin = tmin; }
+    void setTMax(float tmax) const { m_tmax = tmax; }
+    void setMedium(const std::shared_ptr<Medium>& medium) { m_medium = medium; }
 
-    Vec3<float> eval(const float& t) const { return m_origin + m_direction * t; }
+    Vec3<float> eval(const float& t) const {
+        if (t < m_tmin || t > m_tmax) { throw std::out_of_range("Ray::eval: t out of range"); }
+        return m_origin + m_direction * t;
+    }
 
    private:
     Vec3<float> m_origin;
     Vec3<float> m_direction;
+
+    mutable float m_tmin = EPS;
+    mutable float m_tmax = INFINITY;
+    std::weak_ptr<Medium> m_medium;
 };
 
 } // namespace spt
