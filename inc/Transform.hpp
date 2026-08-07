102e775333ee7afecf44ce7f55e0706586d73620
Zhytou <Zhoytou@gmail.com>
2026-08-07 16:25:57 +0800

    add reflect and transmit in Transform

--- inc/Transform.hpp
+++ inc/Transform.hpp
@@ -29,6 +29,28 @@ Mat4x4f rotate(const Vec3f& d);
  */
 Mat4x4f scale(const Vec3f& s);
 
+/**
+ * @brief Calculate the direction of reflected light
+ * 
+ * @param wi Incident direction vector
+ * @param n Normal vector
+ * @param tir Whether to calculate the direction of total internal reflection
+ * 
+ * @return Direction of reflected light
+ */
+Vec3<float> reflect(const Vec3<float>& wi, const Vec3<float>& n, bool tir = false);
+
+/**
+ * @brief Calculate the direction of transmitted light
+ * 
+ * @param wi Incident direction vector
+ * @param n Normal vector
+ * @param eta_i Index of refraction of the medium where the incident light originates
+ * @param eta_t Index of refraction of the medium where the transmitted light arrives
+ * @return Directionmitted direction vector
+ */
+Vec3<float> transmit(const Vec3<float>& wi, const Vec3<float>& n, float eta_i, float eta_t);
+
 } // namespace spt
 
 #endif
--- src/Transform.cpp
+++ src/Transform.cpp
@@ -2,11 +2,11 @@
 
 namespace spt {
 
-Mat4x4f translate(const Vec3f& offset) {
+Mat4x4f translate(const Vec3f& t) {
     Mat4x4f mat = Mat4x4f::eye();
-    mat(0, 3)   = offset[0];
-    mat(1, 3)   = offset[1];
-    mat(2, 3)   = offset[2];
+    mat(0, 3)   = t[0];
+    mat(1, 3)   = t[1];
+    mat(2, 3)   = t[2];
     return mat;
 }
 
@@ -58,6 +58,37 @@ Mat4x4f rotate(const Vec3f& d) {
         mat = rotate(i, radians(d[i])) * mat;
     }
     return mat;
+}
+
+Vec3<float> reflect(const Vec3<float>& wi, const Vec3<float>& n, bool tir) {
+    float cos_theta_i = dot(wi, n);
+    if (cos_theta_i < 0 && !tir) {
+        throw std::runtime_error("reflect: Cosine of incident angle is negative.");
+    }
+    Vec3<float> nn = cos_theta_i > 0 ? n : -n;
+    Vec3<float> wo = 2 * nn * cos_theta_i - wi;
+    return wo;
+}
+
+Vec3<float> transmit(const Vec3<float>& wi, const Vec3<float>& n, float eta_i, float eta_t) {
+    // cosine incident theta
+    float cos_theta_i = dot(wi, n);
+    // relative IOR
+    float eta = cos_theta_i > 0 ? eta_i / eta_t : eta_t / eta_i;
+    // temporary normal vector same hemisphere with wi
+    Vec3<float> nn = cos_theta_i > 0 ? n : -n;
+    // square of sine transmitted theta
+    float sin_theta_t2 = eta * eta * (1 - cos_theta_i * cos_theta_i);
+    // return if total internal reflection occurs
+    if (sin_theta_t2 > 1.f) {
+        throw std::runtime_error("Material::transmit: Total internal reflection occurs.");
+    }
+    // cosine transmitted theta
+    float cos_theta_t = std::sqrt(1.f - sin_theta_t2);
+
+    // snell's law (ior₁·sinθ₁ = ior₂·sinθ₂)
+    Vec3<float> wo = -wi * eta + nn * (eta * cos_theta_i - cos_theta_t);
+    return wo;
 }
 
 } // namespace spt
