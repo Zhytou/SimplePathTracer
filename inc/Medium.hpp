e96f76f310a918700f5a8035de49baa9d2f67b1d
Zhytou <Zhoytou@gmail.com>
2026-08-07 19:00:04 +0800

    update Medium api

--- inc/Medium.hpp
+++ inc/Medium.hpp
@@ -12,20 +12,24 @@ class Medium {
 
     int getID() const { return m_id; }
     const std::string& getName() const { return m_name; }
-    float getIOR() const { return m_ior; }
     Vec3<float> getAbsorption() const { return m_absorption; }
+    Vec3<float> getScattering() const { return m_scattering; }
+    Vec3<float> getExtinction() const { return m_extinction; }
 
     void setID(int id) { m_id = id; }
     void setName(const std::string& name) { m_name = name; }
-    void setIOR(float ior) { m_ior = ior; }
     void setAbsorption(const Vec3<float>& absorption) { m_absorption = absorption; }
+    void setScattering(const Vec3<float>& scattering) { m_scattering = scattering; }
+    void setExtinction(const Vec3<float>& extinction) { m_extinction = extinction; }
 
    private:
     int m_id = -1;
     std::string m_name;
 
-    float m_ior              = 1.0f;
-    Vec3<float> m_absorption = Vec3<float>(1.0f);
+    float m_ior = 1.0f;
+    Vec3<float> m_absorption;
+    Vec3<float> m_scattering;
+    Vec3<float> m_extinction;
 };
 
 } // namespace spt
