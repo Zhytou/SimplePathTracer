dafed5ae3f55259becd622c281c37ad5c31faa5e
Zhytou <Zhoytou@gmail.com>
2026-08-11 14:43:38 +0800

    merge emitter and des

--- inc/Emitter.hpp
+++ inc/Emitter.hpp
@@ -9,85 +9,79 @@ class Primitive;
 
 class Emitter {
    public:
-    Emitter(int id, const Vec3<float>& color) : m_id(id), m_color(color) {}
+    Emitter(int id, const Vec3<float>& radiance) : m_id(id), m_radiance(radiance) {}
     virtual ~Emitter() {}
 
-    virtual bool isDelta() const { return false; }
     int getID() const { return m_id; }
-    Vec3<float> getColor() const { return m_color; }
     std::shared_ptr<Primitive> getPrimitive() const { return m_primitive.lock(); }
     void setID(int id) { m_id = id; }
-    void setColor(const Vec3<float>& color) { m_color = color; }
     void setPrimitive(std::shared_ptr<Primitive> primitive) { m_primitive = primitive; }
 
     /**
-     * @brief Sample a direction from a given shading point to the emitter.
-     * 
-     * @param p The shading point to sample from
-     * @return The sampled direction
-     */
-    virtual Vec3<float> sample(const Vec3<float>& p) const = 0;
+    * @brief  Sample the emitter and return the emitted radiance divided by the PDF.
+    * 
+    * @param point_x The point whose lighting is being evaluated 
+    * @param[out] point_y The sampled point 
+    * @param[out] normal_y The normal on the sampled point
+    * @return The emitted radiance value divided by the probability density of the sampled point.
+    */
+    virtual Vec3<float> sample(const Vec3<float>& point_x, Vec3<float>& point_y, Vec3<float>& normal_y) const = 0;
     /**
-     * @brief Sample a random point directly on the surface of the emitter(ONLY supported for area emitter).
+     * @brief Evaluate the radiance of emitter at a given direction.
      * 
-     * @return The sampled point 
+     * @param wo The direction to evaluate
+     * @return The emitter radiance at the direction
      */
-    virtual Vec3<float> sample() const { return Vec3<float>(NAN); }
+    virtual Vec3<float> eval(const Vec3<float>& dir) const { return m_radiance; }
     /**
-     *  @brief Calculate the probability density function (PDF) for sampling the emitter from a shading point.
+     *  @brief Calculate the probability density function (PDF) in solid angle for sampling the emitter from a shading point.
      * 
-     * @param wo The outgoing direction
-     * @param n The normal vector
-     * @param dis The distance
-     * @return The probability density
+     * @param point_x The point whose lighting is being evaluated 
+     * @param point_y The sampled point 
+     * @param normal_y The normal on the sampled point
+     * @return The probability density of area emitter
      */
-    virtual float pdf(const Vec3<float>& wo, const Vec3<float>& n, float dis) const = 0;
-    /**
-     * @brief Get the area of the emitter
-     * 
-     * @return The area of the emitter
-     */
-    virtual float area() const { return 0.f; }
+    virtual float pdf(const Vec3<float>& point_x, const Vec3<float>& point_y, const Vec3<float>& normal_y) const = 0;
 
    protected:
     int m_id = -1;
-    Vec3<float> m_color{0.f};
+    Vec3<float> m_radiance{0.f};
     std::weak_ptr<Primitive> m_primitive;
 };
 
 class AreaEmitter : public Emitter {
    public:
-    AreaEmitter(int id, const Vec3<float>& color) : Emitter(id, color) {}
+    AreaEmitter(int id, const Vec3<float>& radiance) : Emitter(id, radiance) {}
     ~AreaEmitter() {}
 
-    virtual bool isDelta() const override { return false; }
-    virtual Vec3<float> sample(const Vec3<float>& p) const override;
-    virtual Vec3<float> sample() const override;
-    virtual float pdf(const Vec3<float>& wo, const Vec3<float>& n, float dis) const override;
+    virtual bool isDelta() const { return false; }
+    virtual Vec3<float> sample(const Vec3<float>& point_x, Vec3<float>& point_y, Vec3<float>& normal_y) const override;
+    virtual float pdf(const Vec3<float>& point_x, const Vec3<float>& point_y, const Vec3<float>& normal_y) const override;
 };
 
-class PointEmitter : public Emitter {
+class DES {
    public:
-    PointEmitter(int id, const Vec3<float>& color, const Vec3<float>& position) : Emitter(id, color), m_position(position) {}
-    ~PointEmitter() {}
-
-    virtual Vec3<float> sample(const Vec3<float>& p) const override { return normalize(m_position - p); }
-    virtual float pdf(const Vec3<float>& wo, const Vec3<float>& n, float dis) const override { return INFINITY; }
+    /**
+     * @brief Constructs a Direct Emission Sampler from a collection of scene emitters.
+     * 
+     * @param emitters Vector of emitters (light sources) to be sampled.
+     * @return Shared pointer to the newly created DES instance.
+     */
+    static std::shared_ptr<DES> create(std::vector<std::shared_ptr<Emitter>> emitters);
+    /**
+     * @brief Sample an emitter from the collection of emitters.
+     * 
+     * @return The selected emitter and corresponding probability.
+    */
+    std::pair<std::shared_ptr<Emitter>, float> sample() const;
+    /**
+     * @brief Calculate the selection probability of a emitter.
+     */
+    float prob(std::shared_ptr<Emitter>) const;
 
    private:
-    Vec3<float> m_position;
-};
-
-class DirectionalEmitter : public Emitter {
-   public:
-    DirectionalEmitter(int id, const Vec3<float>& color, const Vec3<float>& direction) : Emitter(id, color), m_direction(normalize(direction)) {}
-    ~DirectionalEmitter() {}
-
-    virtual Vec3<float> sample(const Vec3<float>& p) const override { return -m_direction; }
-    virtual float pdf(const Vec3<float>& wo, const Vec3<float>& n, float dis) const override { return INFINITY; }
-
-   private:
-    Vec3<float> m_direction;
+    std::vector<std::shared_ptr<Emitter>> m_emitters;
+    std::vector<float> m_areas;
 };
 
 } // namespace spt
--- src/Emitter.cpp
+++ src/Emitter.cpp
@@ -3,31 +3,58 @@
 
 namespace spt {
 
-Vec3<float> AreaEmitter::sample(const Vec3<float>& p) const {
+Vec3<float> AreaEmitter::sample(const Vec3<float>& point_x, Vec3<float>& point_y, Vec3<float>& normal_y) const {
     if (m_primitive.expired()) { throw std::runtime_error("AreaEmitter::sample: invalid primitive!"); } // No light source
 
     auto prm = m_primitive.lock();
-    auto pp  = prm->sample();
-    return normalize(pp - p);
+    auto spe = prm->getShape();
+    prm->sample(point_y, normal_y);
+
+    float area = spe->area();
+    float dist = length(point_y - point_x);
+    float cos  = std::abs(dot(normal_y, (point_y - point_x) / dist));
+    if (dist <= EPS) { return Vec3<float>(0.f); }
+
+    return m_radiance * area * cos / (dist * dist);
 }
 
-Vec3<float> AreaEmitter::sample() const {
-    if (m_primitive.expired()) { throw std::runtime_error("AreaEmitter::sample: invalid primitive!"); } // No light source
-
-    auto prm = m_primitive.lock();
-    return prm->sample();
-}
-
-float AreaEmitter::pdf(const Vec3<float>& wo, const Vec3<float>& n, float dis) const {
+float AreaEmitter::pdf(const Vec3<float>& point_x, const Vec3<float>& point_y, const Vec3<float>& normal_y) const {
     if (m_primitive.expired()) { throw std::runtime_error("AreaEmitter::pdf: invalid primitive!"); } // No light source
 
-    auto prm   = m_primitive.lock();
-    float area = prm->getShape()->area();
-    float cos  = std::fabs(dot(n, wo));
-    if (area <= EPS || cos < EPS) { return 0.f; } // downgrade to delta light
+    auto prm = m_primitive.lock();
+    auto spe = prm->getShape();
 
-    float pdf = 1.f / area;         // 1 / dA
-    return pdf * (dis * dis) / cos; // 1 / dw = 1 / dA * dis * dis / cos
+    float area = spe->area();
+    float dist = length(point_y - point_x);
+    float cos  = std::abs(dot(normal_y, (point_y - point_x) / dist));
+    if (area <= EPS || cos < EPS) { return 0.f; }
+
+    float pdf_a = 1.f / area;                  // pdf_area
+    float pdf_w = pdf_a * (dist * dist) / cos; // pdf_solid_angle
+    return pdf_w;
+}
+
+std::shared_ptr<DES> DES::create(std::vector<std::shared_ptr<Emitter>> emitters) {
+    auto des = std::make_shared<DES>();
+    for (auto emitter : emitters) {
+        auto area = emitter->getPrimitive()->getShape()->area();
+        des->m_emitters.push_back(emitter);
+        des->m_areas.push_back((des->m_areas.empty() ? 0.f : des->m_areas.back()) + area);
+    }
+    return des;
+}
+
+std::pair<std::shared_ptr<Emitter>, float> DES::sample() const {
+    float x  = rand(0.f, m_areas.back());
+    auto itr = std::lower_bound(m_areas.begin(), m_areas.end(), x);
+    int i    = std::distance(m_areas.begin(), itr);
+    return std::make_pair(m_emitters[i], m_areas[i] / m_areas.back());
+}
+
+float DES::prob(std::shared_ptr<Emitter> emitter) const {
+    if (emitter == nullptr || m_emitters.empty()) { return 0.f; }
+
+    return emitter->getPrimitive()->getShape()->area() / m_areas.back();
 }
 
 } // namespace spt
